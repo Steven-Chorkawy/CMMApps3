bd60572ae8d2f0f4c3ab219bc4aa19eae124bb46
Steven Chorkawy <sc13@clarington.net>
2023-02-21 08:56:47 -0500

    first commit

---
+++ src/webparts/newCommitteeMember/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface INewCommitteeMemberWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'NewCommitteeMemberWebPartStrings' {
+  const strings: INewCommitteeMemberWebPartStrings;
+  export = strings;
+}
