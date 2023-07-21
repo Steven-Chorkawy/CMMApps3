bde31b2d7b57426d512596a793696f6ab14fda07
Steven Chorkawy <schorkawy@clarington.net>
2023-07-21 11:53:30 -0400

    Made a RenewMemberComponent file.

--- src/ClaringtonComponents/RenewCommitteeMember.tsx
+++ src/ClaringtonComponents/RenewCommitteeMember.tsx
@@ -2,8 +2,11 @@ import { WebPartContext } from '@microsoft/sp-webpart-base';
 import { Panel, PanelType } from 'office-ui-fabric-react';
 import * as React from 'react';
 import IMemberListItem from '../ClaringtonInterfaces/IMemberListItem';
-import { GetMember } from '../HelperMethods/MyHelperMethods';
+import { GetMember, OnFormatDate } from '../HelperMethods/MyHelperMethods';
 import { MyShimmer } from './MyShimmer';
+import { Dashboard, WidgetSize } from '@pnp/spfx-controls-react/lib/Dashboard';
+import { CommitteeMemberContactDetails, CommitteeMemberTermHistory } from './MemberDetailsComponent';
+import { RenewMemberComponent } from './RenewMemberComponent';
 
 export interface IRenewCommitteeMemberProps {
     description?: string;
@@ -29,13 +32,56 @@ export class RenewCommitteeMember extends React.Component<IRenewCommitteeMemberP
     }
 
     public render(): React.ReactElement<any> {
+        const LINK_MEMBER_EDIT_FORM = { href: `/sites/CMM/Lists/Members/EditForm.aspx?ID=${this.props.memberId}` };
+        const LINK_MEMBER_HISTORY = { href: `/sites/CMM/Lists/CommitteeMemberHistory?FilterField1=MemberID&FilterValue1=${this.props.memberId}&sortField=_EndDate&isAscending=false` };
+        const LINK_EXAMPLE = { href: `#` };
         if (this.state.selectedMember) {
             return (
                 <div>
-                    <div>hello world... Member ID: {this.props.memberId}</div>
+                    <div>
+                        <Dashboard
+                            widgets={[
+                                {
+                                    title: "Renew Committee Member",
+                                    size: WidgetSize.Triple,
+                                    body: [{
+                                        id: 'renewMemberId',
+                                        title: 'Renew Member',
+                                        content: (<RenewMemberComponent />)
+                                    }]
+                                },
+                                {
+                                    title: this.state.selectedMember.Title,
+                                    desc: `Last updated ${OnFormatDate(new Date(this.state.selectedMember.Modified))}`,
+                                    size: WidgetSize.Single,
+                                    body: [
+                                        {
+                                            id: "t1",
+                                            title: "Tab 1",
+                                            content: (
+                                                <CommitteeMemberContactDetails member={this.state.selectedMember} />
+                                            ),
+                                        }
+                                    ],
+                                    link: LINK_MEMBER_EDIT_FORM,
+                                },
+                                // {
+                                //     title: "Committee History",
+                                //     size: WidgetSize.Single,
+                                //     body: [{
+                                //         id: 'id',
+                                //         title: 'Committee History',
+                                //         content: (<CommitteeMemberTermHistory memberID={this.state.selectedMember.ID} context={this.props.context} />)
+                                //     }],
+                                //     link: LINK_MEMBER_HISTORY,
+                                // },
+
+                            ]} />
+                    </div>
+                    {/* <hr />
                     <div>
                         {this.state.selectedMember && JSON.stringify(this.state.selectedMember)}
-                    </div>
+                    </div> */}
                 </div>
             );
         }
---
+++ src/ClaringtonComponents/RenewMemberComponent.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+export class RenewMemberComponent extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+
+    }
+
+    public render(): React.ReactElement<any> { 
+        return <div>RenewMemberComponent...</div>
+    }
+}
